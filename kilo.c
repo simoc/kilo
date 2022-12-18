0b61f4eb295a8ca88628e8f71e9f97ec9844a0a1
Simon Chenery <simon_chenery@yahoo.com>
2022-12-18 10:04:32 +0100

    Implemented step 5 - turn off echoing

--- kilo.c
+++ kilo.c
@@ -1,9 +1,22 @@
 
+#include <termios.h>
 #include <unistd.h>
+
+void
+enableRawMode(void)
+{
+	struct termios raw;
+
+	tcgetattr(STDIN_FILENO, &raw);
+	raw.c_lflag &= ~(ECHO);
+	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
 
 int
 main(int argc, char *argv[])
 {
+	enableRawMode();
+
 	char c;
 	while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q')
 	{
