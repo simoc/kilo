fc6d88235305ce9a9aed6d6e0ae672bc630408aa
Simon Chenery <simon_chenery@yahoo.com>
2022-12-18 10:45:29 +0100

    Implemented step 8 - display keypresses

--- kilo.c
+++ kilo.c
@@ -1,6 +1,8 @@
 
 #include <termios.h>
 #include <unistd.h>
+#include <ctype.h>
+#include <stdio.h>
 #include <stdlib.h>
 
 struct termios orig_termios;
@@ -30,6 +32,14 @@ main(int argc, char *argv[])
 	char c;
 	while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q')
 	{
+		if (iscntrl(c))
+		{
+			printf("%d\n", c);
+		}
+		else
+		{
+			printf("%d ('%c')\n", c, c);
+		}
 	}
 	return 0;
 }
