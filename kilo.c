3d7bac21ede8b6bbd66c5cff5ec46fb1eb875251
Simon Chenery <simon_chenery@yahoo.com>
2022-12-19 16:24:54 +0100

    Implemented step 14 - add carriage-returns

--- kilo.c
+++ kilo.c
@@ -39,11 +39,11 @@ main(int argc, char *argv[])
 	{
 		if (iscntrl(c))
 		{
-			printf("%d\n", c);
+			printf("%d\r\n", c);
 		}
 		else
 		{
-			printf("%d ('%c')\n", c, c);
+			printf("%d ('%c')\r\n", c, c);
 		}
 	}
 	return 0;
