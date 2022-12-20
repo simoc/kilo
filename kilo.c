e1b349f19ef654f892736d36b0c550cc11192322
Simon Chenery <simon_chenery@yahoo.com>
2022-12-20 21:56:41 +0100

    Implemented step 25 - print tildes

--- kilo.c
+++ kilo.c
@@ -95,11 +95,24 @@ editorProcessKeypress(void)
 /*** output ***/
 
 void
+editorDrawRows(void)
+{
+	int y;
+
+	for (y = 0; y < 24; y++)
+	{
+		write(STDOUT_FILENO, "~\r\n", 3);
+	}
+}
+
+void
 editorRefreshScreen(void)
 {
 	/* clear screen */
 	write(STDOUT_FILENO, "\x1b[2J", 4);
 	/* move cursor to top left */
+	write(STDOUT_FILENO, "\x1b[H", 3);
+	editorDrawRows();
 	write(STDOUT_FILENO, "\x1b[H", 3);
 }
 
