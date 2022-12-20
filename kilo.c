942a30a228f04a1acb28d35bdc559fc7778a476c
Simon Chenery <simon_chenery@yahoo.com>
2022-12-20 19:38:39 +0100

    Implemented step 21 - refactor keyboard input

--- kilo.c
+++ kilo.c
@@ -57,6 +57,37 @@ enableRawMode(void)
 	}
 }
 
+char
+editorReadKey(void)
+{
+	int nread;
+	char c = '\0';
+
+	while ((nread = read(STDIN_FILENO, &c, 1)) != 1)
+	{
+		if (nread == -1 && errno != EAGAIN)
+		{
+			die("read");
+		}
+	}
+	return c;
+}
+
+/*** input ***/
+
+void
+editorProcessKeypress(void)
+{
+	char c = editorReadKey();
+
+	switch (c)
+	{
+	case CTRL_KEY('q'):
+		exit(0);
+		break;
+	}
+}
+
 /*** init ***/
 int
 main(int argc, char *argv[])
@@ -65,23 +96,7 @@ main(int argc, char *argv[])
 
 	while (1)
 	{
-		char c = '\0';
-		if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN)
-		{
-			die("read");
-		}
-		if (iscntrl(c))
-		{
-			printf("%d\r\n", c);
-		}
-		else
-		{
-			printf("%d ('%c')\r\n", c, c);
-		}
-		if (c == CTRL_KEY('q'))
-		{
-			break;
-		}
+		editorProcessKeypress();
 	}
 	return 0;
 }
