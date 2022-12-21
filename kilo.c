78fea21dab45cc47432bb8a175b7ce2543afee5b
Simon Chenery <simon_chenery@yahoo.com>
2022-12-21 21:51:36 +0100

    Step 37 - add abAppend, abFree

--- kilo.c
+++ kilo.c
@@ -5,6 +5,7 @@
 #include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /*** defines ***/
 #define CTRL_KEY(k) ((k) & 0x1f)
@@ -156,6 +157,26 @@ struct abuf
 };
 
 #define ABUF_INIT {NULL, 0}
+
+void
+abAppend(struct abuf *ab, const char *s, int len)
+{
+	char *new = realloc(ab->b, ab->len + len);
+
+	if (new == NULL)
+	{
+		return;
+	}
+	memcpy(&new[ab->len], s, len);
+	ab->b = new;
+	ab->len += len;
+}
+
+void
+abFree(struct abuf *ab)
+{
+	free(ab->b);
+}
 
 /*** input ***/
 
