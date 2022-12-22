03362fa4c564c794f8a5435dc72232c393b44242
Simon Chenery <simon_chenery@yahoo.com>
2022-12-22 20:55:21 +0100

    Step 52 - detect home, end keys

--- kilo.c
+++ kilo.c
@@ -19,6 +19,8 @@ enum editorKey
 	ARROW_RIGHT,
 	ARROW_DOWN,
 	ARROW_UP,
+	HOME_KEY,
+	END_KEY,
 	PAGE_UP,
 	PAGE_DOWN
 };
@@ -122,10 +124,18 @@ editorReadKey(void)
 				{
 					switch (seq[1])
 					{
+					case '1':
+						return HOME_KEY;
+					case '4':
+						return END_KEY;
 					case '5':
 						return PAGE_UP;
 					case '6':
 						return PAGE_DOWN;
+					case '7':
+						return HOME_KEY;
+					case '8':
+						return END_KEY;
 					}
 				}
 			}
@@ -141,7 +151,21 @@ editorReadKey(void)
 					return ARROW_RIGHT;
 				case 'D':
 					return ARROW_LEFT;
+				case 'H':
+					return HOME_KEY;
+				case 'F':
+					return END_KEY;
 				}
+			}
+		}
+		else if (seq[0] == 'O')
+		{
+			switch (seq[1])
+			{
+			case 'H':
+				return HOME_KEY;
+			case 'F':
+				return END_KEY;
 			}
 		}
 
