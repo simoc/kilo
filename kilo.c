1db4a11a963849da806d0ead2ef80591ca28c01c
Simon Chenery <simon_chenery@yahoo.com>
2022-12-22 20:42:42 +0100

    Step 50 - detect page up/down

--- kilo.c
+++ kilo.c
@@ -18,7 +18,9 @@ enum editorKey
 	ARROW_LEFT = 1000,
 	ARROW_RIGHT,
 	ARROW_DOWN,
-	ARROW_UP
+	ARROW_UP,
+	PAGE_UP,
+	PAGE_DOWN
 };
 
 /*** data ***/
@@ -110,16 +112,36 @@ editorReadKey(void)
 
 		if (seq[0] == '[')
 		{
-			switch (seq[1])
+			if (seq[1] >= '0' && seq[1] <= '9')
 			{
-			case 'A':
-				return ARROW_UP;
-			case 'B':
-				return ARROW_DOWN;
-			case 'C':
-				return ARROW_RIGHT;
-			case 'D':
-				return ARROW_LEFT;
+				if (read(STDIN_FILENO, &seq[2], 1) != 1)
+				{
+					return '\x1b';
+				}
+				if (seq[2] == '~')
+				{
+					switch (seq[1])
+					{
+					case '5':
+						return PAGE_UP;
+					case '6':
+						return PAGE_DOWN;
+					}
+				}
+			}
+			else
+			{
+				switch (seq[1])
+				{
+				case 'A':
+					return ARROW_UP;
+				case 'B':
+					return ARROW_DOWN;
+				case 'C':
+					return ARROW_RIGHT;
+				case 'D':
+					return ARROW_LEFT;
+				}
 			}
 		}
 
