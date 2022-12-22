981f65765c68b8e5b3c36ecca5cd7a8b434d8058
Simon Chenery <simon_chenery@yahoo.com>
2022-12-22 21:03:32 +0100

    Step 53 - simple home, end handling

--- kilo.c
+++ kilo.c
@@ -319,6 +319,14 @@ editorProcessKeypress(void)
 		exit(0);
 		break;
 
+	case HOME_KEY:
+		E.cx = 0;
+		break;
+
+	case END_KEY:
+		E.cx = E.screencols - 1;
+		break;
+
 	case PAGE_UP:
 	case PAGE_DOWN:
 		{
