fff273a2c569f0ba602fd9fb688fac209940750a
Simon Chenery <simon_chenery@yahoo.com>
2022-12-23 07:07:38 +0100

    Step 58 - open text file

--- kilo.c
+++ kilo.c
@@ -257,16 +257,34 @@ getWindowSize(int *rows, int *cols)
 /*** file i/o ***/
 
 void
-editorOpen(void)
+editorOpen(char *filename)
 {
-	char *line = "Hello, World!";
-	ssize_t linelen = 13;
+	FILE *fp = fopen(filename, "r");
+	if (!fp)
+	{
+		die("fopen");
+	}
 
-	E.row.size = linelen;
-	E.row.chars = malloc(linelen + 1);
-	memcpy(E.row.chars, line, linelen);
-	E.row.chars[linelen] = '\0';
-	E.numrows = 1;
+	char *line = NULL;
+	size_t linecap = 0;
+	ssize_t linelen;
+	linelen = getline(&line, &linecap, fp);
+	if (linelen != -1)
+	{
+		while (linelen > 0 &&
+			(line[linelen - 1] == '\r' || line[linelen - 1] == '\n'))
+		{
+			linelen--;
+		}
+
+		E.row.size = linelen;
+		E.row.chars = malloc(linelen + 1);
+		memcpy(E.row.chars, line, linelen);
+		E.row.chars[linelen] = '\0';
+		E.numrows = 1;
+	}
+	free(line);
+	fclose(fp);
 }
 
 /*** append buffer ***/
@@ -469,7 +487,10 @@ main(int argc, char *argv[])
 {
 	enableRawMode();
 	initEditor();
-	editorOpen();
+	if (argc >= 2)
+	{
+		editorOpen(argv[1]);
+	}
 
 	while (1)
 	{
