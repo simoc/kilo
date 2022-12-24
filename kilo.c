e9d5bdc7ec2a75cf2373d7069dfd2ef25b3482cc
Simon Chenery <simon_chenery@yahoo.com>
2022-12-24 19:16:46 +0100

    Step 73 - editor scroll horizontal

--- kilo.c
+++ kilo.c
@@ -423,6 +423,14 @@ editorScroll(void)
 	{
 		E.rowoff = E.cy - E.screenrows + 1;
 	}
+	if (E.cx < E.coloff)
+	{
+		E.coloff = E.cx;
+	}
+	if (E.cx >= E.coloff + E.screencols)
+	{
+		E.coloff = E.cx - E.screencols + 1;
+	}
 }
 
 void
