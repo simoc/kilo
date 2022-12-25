54d30008df5f2ec56b64e6222cb6e1e4de7acf35
Simon Chenery <simon_chenery@yahoo.com>
2022-12-25 08:17:12 +0100

    Step 96 - display line number in status line

--- kilo.c
+++ kilo.c
@@ -601,8 +601,11 @@ editorDrawStatusBar(struct abuf *ab)
 	/* invert colours */
 	abAppend(ab, "\x1b[7m", 4);
 	char status[80];
+	char rstatus[80];
 	int len = snprintf(status, sizeof(status), "%.20s - %d lines",
 		E.filename ? E.filename : "[No Name]", E.numrows);
+	int rlen = snprintf(rstatus, sizeof(rstatus), "%d/%d",
+		E.cy + 1, E.numrows);
 	if (len > E.screencols)
 	{
 		len = E.screencols;
@@ -610,7 +613,15 @@ editorDrawStatusBar(struct abuf *ab)
 	abAppend(ab, status, len);
 	while (len < E.screencols)
 	{
-		abAppend(ab, " ", 1);
+		if (E.screencols - len == rlen)
+		{
+			abAppend(ab, rstatus, rlen);
+			break;
+		}
+		else
+		{
+			abAppend(ab, " ", 1);
+		}
 		len++;
 	}
 	/* restore normal colours */
