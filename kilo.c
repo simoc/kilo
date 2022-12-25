7242dc505948c13f942054b92c4bfe6d8de8e326
Simon Chenery <simon_chenery@yahoo.com>
2022-12-25 21:52:45 +0100

    Step 116 - add editorRowDelChar

--- kilo.c
+++ kilo.c
@@ -371,6 +371,19 @@ editorRowInsertChar(erow *row, int at, int c)
 	E.dirty++;
 }
 
+void
+editorRowDelChar(erow *row, int at)
+{
+	if (at < 0 || at >= row->size)
+	{
+		return;
+	}
+	memmove(&row->chars[at], &row->chars[at + 1], row->size - at);
+	row->size--;
+	editorUpdateRow(row);
+	E.dirty++;
+}
+
 /*** editor operations ***/
 
 void
