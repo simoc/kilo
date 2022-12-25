076c22bb9717c6a726f04ce0bc5a7835bbbac7f3
Simon Chenery <simon_chenery@yahoo.com>
2022-12-25 21:21:28 +0100

    Step 115 - confirm quit with unsaved changes

--- kilo.c
+++ kilo.c
@@ -20,6 +20,7 @@
 /*** defines ***/
 #define KILO_VERSION "0.0.1"
 #define KILO_TAB_STOP 8
+#define KILO_QUIT_TIMES 3
 
 #define CTRL_KEY(k) ((k) & 0x1f)
 
@@ -556,6 +557,8 @@ editorMoveCursor(int key)
 void
 editorProcessKeypress(void)
 {
+	static int quit_times = KILO_QUIT_TIMES;
+
 	int c = editorReadKey();
 
 	switch (c)
@@ -565,6 +568,13 @@ editorProcessKeypress(void)
 		break;
 
 	case CTRL_KEY('q'):
+		if (E.dirty > 0 && quit_times > 0)
+		{
+			editorSetStatusMessage("WARNING!!! File has unsaved changes."
+				" Press Ctrl-Q %d more times to quit.", quit_times);
+			quit_times--;
+			return;
+		}
 		write(STDOUT_FILENO, "\x1b[2J", 4);
 		write(STDOUT_FILENO, "\x1b[H", 3);
 		exit(0);
@@ -629,6 +639,8 @@ editorProcessKeypress(void)
 		editorInsertChar(c);
 		break;
 	}
+
+	quit_times = KILO_QUIT_TIMES;
 }
 
 /*** output ***/
