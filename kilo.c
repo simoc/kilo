9d192de2ddf8f8116b33ff75f82d325298edd6fe
Simon Chenery <simon_chenery@yahoo.com>
2022-12-25 20:46:30 +0100

    Step 111 - add editorConfig.dirty

--- kilo.c
+++ kilo.c
@@ -62,6 +62,8 @@ struct editorConfig
 	int screencols;
 	int numrows;
 	erow *row;
+	/* is file changed since last modification? */
+	int dirty;
 	char *filename;
 	char statusmsg[80];
 	time_t statusmsg_time;
@@ -808,6 +810,7 @@ initEditor(void)
 	E.coloff = 0;
 	E.numrows = 0;
 	E.row = NULL;
+	E.dirty = 0;
 	E.filename = NULL;
 	E.statusmsg[0] = '\0';
 	E.statusmsg_time = 0;
