fda17e225f2873e400b54d8d87066c29a3a8b44d
Simon Chenery <simon_chenery@yahoo.com>
2022-12-26 20:49:31 +0100

    Step 120 - add editorRowAppendString

--- kilo.c
+++ kilo.c
@@ -392,6 +392,17 @@ editorRowInsertChar(erow *row, int at, int c)
 }
 
 void
+editorRowAppendString(erow *row, char *s, size_t len)
+{
+	row->chars = realloc(row->chars, row->size + len + 1);
+	memcpy(&row->chars[row->size], s, len);
+	row->size += len;
+	row->chars[row->size] = '\0';
+	editorUpdateRow(row);
+	E.dirty++;
+}
+
+void
 editorRowDelChar(erow *row, int at)
 {
 	if (at < 0 || at >= row->size)
