9fa28f28151debc0aed46d69d722ecb2d2aa3ec4
Simon Chenery <simon_chenery@yahoo.com>
2022-12-26 20:59:41 +0100

    Step 121 - delete row when empty

--- kilo.c
+++ kilo.c
@@ -436,12 +436,23 @@ editorDelChar(void)
 	{
 		return;
 	}
+	if (E.cx == 0 && E.cy == 0)
+	{
+		return;
+	}
 
 	erow *row = &E.row[E.cy];
 	if (E.cx > 0)
 	{
 		editorRowDelChar(row, E.cx - 1);
 		E.cx--;
+	}
+	else
+	{
+		E.cx = E.row[E.cy - 1].size;
+		editorRowAppendString(&E.row[E.cy - 1], row->chars, row->size);
+		editorDelRow(E.cy);
+		E.cy--;
 	}
 }
 
