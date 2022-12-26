13bd9a1ebf203a44d6f4be9fb9edf0c89030a6b0
Simon Chenery <simon_chenery@yahoo.com>
2022-12-26 08:05:49 +0100

    Step 118 - implement backspace and delete

--- kilo.c
+++ kilo.c
@@ -627,7 +627,11 @@ editorProcessKeypress(void)
 	case BACKSPACE:
 	case CTRL_KEY('h'):
 	case DEL_KEY:
-		/* TODO */
+		if (c == DEL_KEY)
+		{
+			editorMoveCursor(ARROW_RIGHT);
+		}
+		editorDelChar();
 		break;
 
 	case PAGE_UP:
