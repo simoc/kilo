c1d16893edbd65c5f9c400db51793791e67ed764
Simon Chenery <simon_chenery@yahoo.com>
2022-12-30 07:40:37 +0100

    Step 129 - cancel save filename entry

--- kilo.c
+++ kilo.c
@@ -535,7 +535,12 @@ editorSave(void)
 {
 	if (E.filename == NULL)
 	{
-		E.filename = editorPrompt("Save as: %s");
+		E.filename = editorPrompt("Save as: %s (ESC to cancel)");
+		if (E.filename == NULL)
+		{
+			editorSetStatusMessage("Save aborted");
+			return;
+		}
 	}
 
 	int len;
