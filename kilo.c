2552e18e367148652efcb1d9cacc21fdfe37fb44
Simon Chenery <simon_chenery@yahoo.com>
2022-12-31 02:16:41 +0100

    Step 143 - syntax highlighting for digits

--- kilo.c
+++ kilo.c
@@ -986,7 +986,21 @@ editorDrawRows(struct abuf *ab)
 			{
 				len = E.screencols;
 			}
-			abAppend(ab, &E.row[filerow].render[E.coloff], len);
+			char *c = &E.row[filerow].render[E.coloff];
+			int j;
+			for (j = 0; j < len; j++)
+			{
+				if (isdigit(c[j]))
+				{
+					abAppend(ab, "\x1b[31m", 5);
+					abAppend(ab, &c[j], 1);
+					abAppend(ab, "\x1b[39m", 5);
+				}
+				else
+				{
+					abAppend(ab, &c[j], 1);
+				}
+			}
 		}
 
 		abAppend(ab, "\x1b[K", 3);
