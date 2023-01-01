3b178d0f54166dbf2f55241d5d31bd131bbf7a56
Simon Chenery <simon_chenery@yahoo.com>
2023-01-01 17:19:12 +0100

    Step 173 - add list of C keywords for syntax highlighting

--- kilo.c
+++ kilo.c
@@ -33,6 +33,7 @@ struct editorSyntax
 {
 	char *filetype;
 	char **filematch;
+	char **keywords;
 	char *singleline_comment_start;
 	int flags;
 };
@@ -102,11 +103,21 @@ struct editorConfig E;
 
 char *C_HL_extensions[] = {".c", ".h", ".cpp", NULL};
 
+char *C_HL_keywords[] =
+{
+	"switch", "if", "while", "for", "break", "continue", "return", "else",
+	"struct", "union", "typedef", "static", "enum", "class", "case",
+
+	"int|", "long|", "double|", "float|", "char|", "unsigned|", "signed|",
+	"void|", NULL
+};
+
 struct editorSyntax HLDB[] =
 {
 	{
 		"c",
 		C_HL_extensions,
+		C_HL_keywords,
 		"//",
 		HL_HIGHLIGHT_NUMBERS | HL_HIGHLIGHT_STRINGS
 	},
