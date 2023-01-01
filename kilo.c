6aaaa4f53c5e991d43fee18372ffa0e5af617872
Simon Chenery <simon_chenery@yahoo.com>
2023-01-01 14:09:21 +0100

    Step 170 - add syntax highlighting for single line comments

--- kilo.c
+++ kilo.c
@@ -335,6 +335,9 @@ editorUpdateSyntax(erow *row)
 		return;
 	}
 
+	char *scs = E.syntax->singleline_comment_start;
+	int scs_len = scs ? strlen(scs) : 0;
+
 	int prev_sep = 1;
 	int in_string = 0;
 
@@ -343,6 +346,16 @@ editorUpdateSyntax(erow *row)
 	{
 		char c = row->render[i];
 		unsigned char prev_hl = (i > 0) ? row->hl[i - 1] : HL_NORMAL;
+
+		if (scs_len > 0 && !in_string)
+		{
+			if (strncmp(&row->render[i], scs, scs_len) == 0)
+			{
+				/* highlight comment line */
+				memset(&row->hl[i], HL_COMMENT, row->rsize - i);
+				break;
+			}
+		}
 
 		if (E.syntax->flags & HL_HIGHLIGHT_STRINGS)
 		{
